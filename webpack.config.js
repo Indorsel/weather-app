1bc99a5f1f3b8a7352e5e630383d66b8fdb5387a
Timur <timoha993@gmail.com>
2021-03-26 11:39:37 +0300

    deploy my weather app to github pages

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   plugins: [new HtmlWebpackPlugin(), new webpack.HotModuleReplacementPlugin(), new MiniCssExtractPlugin()],
